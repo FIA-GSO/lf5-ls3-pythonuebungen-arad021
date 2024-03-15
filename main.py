8d2401b3f7cf3e6ad196eb98d67de575d45cc298
Arad Ahmad <arad.ahmad@check24.de>
2024-03-15 12:21:55 +0100

    -Aufgabe 1 erledigt

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 # This is a sample Python script.
 # Press Umschalt+F10 to execute it or replace it with your code.
+import math
+
 
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#---------------------Aufgabe 1 ------------------------------------
-def compute_r2d2_population(steps: int) -> tuple[int,int,int]:
-    """
-        Computes the r2d2 population for the given step amount
-    :param steps: amount of steps to compute the population (e.g.: 5)
-    :return: tuple of childs adults and old r2d2
-    """
-    return (0,0,0)
+# ---------------------Aufgabe 1 ------------------------------------
+def compute_r2d2_population(steps: int) -> tuple[int, int, int]:
+    young = 10
+    adults = 10
+    old = 10
+    i = 0
 
-#---------------------Aufgabe 2 Streichholz------------------------------
-#IMPLEMENT YOUR SOLUTION FOR THE STEICHHOLZSPIEL HERE
+    for x in range(0, steps):
+        oldOld = old
+        oldAdults = adults
+        oldYoung = young
+
+        old = math.floor(oldAdults / 3)
+        adults = oldYoung / 2
+        young = (oldOld * 2) + (oldAdults * 4)
+
+    return young, adults, old
 
 
-#---------------------Aufgabe 3 Heron ------------------------------------
-def heron_verfahren(area : float, threshold:float) -> float:
+# ---------------------Aufgabe 2 Streichholz------------------------------
+# IMPLEMENT YOUR SOLUTION FOR THE STEICHHOLZSPIEL HERE
+
+
+# ---------------------Aufgabe 3 Heron ------------------------------------
+def heron_verfahren(area: float, threshold: float) -> float:
     """
         computes the square root using the heron method
     :param area: size of the area e.g.25
@@ -27,12 +39,12 @@ def heron_verfahren(area : float, threshold:float) -> float:
     return 0
 
 
-#---------------------Aufgabe 4 Quersumme------------------------------
-#IMPLEMENT, IF NECESSARY, EXERCISE 4 HERE BUT USE A FUNCTION!
+# ---------------------Aufgabe 4 Quersumme------------------------------
+# IMPLEMENT, IF NECESSARY, EXERCISE 4 HERE BUT USE A FUNCTION!
 
 
-#---------------MANAGEMENT----------------------
-#-------------COMMENT/UNCOMMENT lines to launch the different exercises
+# ---------------MANAGEMENT----------------------
+# -------------COMMENT/UNCOMMENT lines to launch the different exercises
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("You need to adjust this code to run your implementation")
@@ -49,12 +61,11 @@ if __name__ == '__main__':
     # TO BE IMPLEMENTED
 
     # Aufgabe 3
-    print (f"Die Wurzel für die Fläche 25 und Grenze 0.01 nach Heron ist: {heron_verfahren(25, 0.01)}")
+    print(f"Die Wurzel für die Fläche 25 und Grenze 0.01 nach Heron ist: {heron_verfahren(25, 0.01)}")
 
     # Aufgabe 4
     # TO BE IMPLEMENTED
 
     # Use a breakpoint in the code line below to debug your script.
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
